437e11cd9e284a7f6310041b6510ef7bdbf9ba81
Park,JongBum <jbpark03@gmail.com>
2021-07-04 15:21:36 +0900

    search key toString method defined

--- src/com/jbpark/dabang/module/AddrSearchKey.java
+++ src/com/jbpark/dabang/module/AddrSearchKey.java
@@ -4,22 +4,41 @@ public class AddrSearchKey {
 	private String 도로명일부;
 	private String 건물명일부;
 	private String 건물본번일부;
+
 	public String get도로명일부() {
 		return 도로명일부;
 	}
+
 	public void set도로명일부(String 도로명일부) {
 		this.도로명일부 = 도로명일부;
 	}
+
 	public String get건물명일부() {
 		return 건물명일부;
 	}
+
 	public void set건물명일부(String 건물명일부) {
 		this.건물명일부 = 건물명일부;
 	}
+
 	public String get건물본번일부() {
 		return 건물본번일부;
 	}
+
 	public void set건물본번일부(String 건물본번일부) {
 		this.건물본번일부 = 건물본번일부;
 	}
+
+	//@formatter:off
+	@Override
+	public String toString() {
+		return "검색키: [도로명='" + 
+				(도로명일부 == null ? "" : 도로명일부)
+				+ "', 건물명='" + 
+				(건물명일부 == null ? "" : 건물명일부)
+				+ "', 건물본번='" +
+				(건물본번일부 == null ? "" : 건물본번일부) 
+				+ "']";
+	}
+	
 }
