e851a49673e5d809df8e496e4f38e2f6c1e38923
Park, JongBum <jbpark03@gmail.com>
2021-07-05 15:13:32 +0900

    fix again

--- src/com/jbpark/dabang/module/AddressMan.java
+++ src/com/jbpark/dabang/module/AddressMan.java
@@ -125,9 +125,9 @@ public class AddressMan {
 			AddrSearchKey addrSearchKey = getAddrSearchKey(scanner);
 
 			boolean cond1 = addrSearchKey.get도로명일부() != null;
-			boolean cond2 = addrSearchKey.get건물명일부() != null;
-			boolean cond3 = addrSearchKey.get도로명일부() != null 
+			boolean cond2 = addrSearchKey.get도로명일부() != null
 					&& addrSearchKey.get건물본번일부() != null;
+			boolean cond3 = addrSearchKey.get건물명일부() != null;
 			
 			assert ( cond1 || cond2 || cond3)
 				: "건물본번 있으나 도로명은 없음!";
