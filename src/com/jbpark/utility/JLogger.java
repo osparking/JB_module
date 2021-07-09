462e40b630fb3d5d88466722a6581249617ca040
Park,JongBum <jbpark03@gmail.com>
2021-07-09 21:41:38 +0900

    log files goes to project 'LOG' folder

--- src/com/jbpark/utility/JLogger.java
+++ src/com/jbpark/utility/JLogger.java
@@ -19,20 +19,39 @@ public class JLogger {
 	private static Logger logger = Logger.getGlobal();
 	
 	public static String getFilePath() {
-		return "로그파일: " + logFile;
+		return "로그: " + logFile;
 	}
-	private static String logFile = "D:/LOG/global.%g.log";
+	private static String logFile = "";
+	
+	/**
+	 * 로그파일 이름 'corejava.%g.log' 사용됨.
+	 * @return 맞춤 변형된 Global logger 
+	 */
 	public static Logger getLogger() {
+		return getLogger("corejava");
+	}
+	/**
+	 * 로그파일 이름 '<filename>.%g.log' 사용됨.
+	 * @param filename
+	 * @return 맞춤 변형된 Global logger 
+	 */
+	public static Logger getLogger(String filename) {
+		setLogFile(filename);
+		
 		logger.setLevel(Level.CONFIG);
 		logger.setUseParentHandlers(false);
-		int LOG_ROTATION_COUNT = 30;
+		int LOG_ROTATION_COUNT = 3;
 		JB_FileHandler handler;
+		//@formatter:off
 		try {
-			handler = new JB_FileHandler(logFile, 0, LOG_ROTATION_COUNT);
+			handler = new JB_FileHandler(logFile, 0,
+					LOG_ROTATION_COUNT);
 			handler.setLevel(Level.CONFIG);
 			handler.setFormatter(new SimpleFormatter());
 			logger.addHandler(handler);
 		} catch (NoSuchFileException e) {
+			System.out.println("사용자 폴더: " 
+					+ System.getProperty("user.dir"));
 			System.out.println("파일부재 오류: " + e.getFile());
 			System.out.println("프로그램 종료!");
 			System.exit(-1);
@@ -42,5 +61,8 @@ public class JLogger {
 		
 		System.out.println(JLogger.getFilePath());
 		return logger;
+	}
+	private static void setLogFile(String filename) {
+		logFile = "LOG/" + filename + ".%g.log";
 	}	
 }
