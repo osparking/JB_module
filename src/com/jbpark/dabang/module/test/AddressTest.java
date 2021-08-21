aa7c1ebd68b6363b000888b4167f53484f5a28d9
Park,JongBum <jbpark03@gmail.com>
2021-08-21 12:28:21 +0900

    고객 주소 목록 표시 모듈 test case 수정

--- src/com/jbpark/dabang/module/test/AddressTest.java
+++ src/com/jbpark/dabang/module/test/AddressTest.java
@@ -2,12 +2,16 @@ package com.jbpark.dabang.module.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.logging.Logger;
+
 import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 
 import com.jbpark.dabang.module.AddrSearchKey;
 import com.jbpark.dabang.module.AddressMan;
 import com.jbpark.dabang.module.SearchResult;
 import com.jbpark.dabang.module.StopSearchingException;
+import com.jbpark.utility.JLogger;
 
 public class AddressTest {
 	AddressMan aMan = new AddressMan();
@@ -18,8 +22,8 @@ public class AddressTest {
 		
 		key.set도로_건물("팔달");
 		try {
-			SearchResult result = aMan.searchAddress(key, 1);
-			int count = aMan.getTotalRows(key);
+			SearchResult result = AddressMan.searchAddress(key, 1);
+			int count = AddressMan.getTotalRows(key);
 			assertTrue(count == 2045 && result.getAddrCount() == 20);			
 		} catch (StopSearchingException e) {
 			// TODO Auto-generated catch block
@@ -27,10 +31,12 @@ public class AddressTest {
 		}
 	}
 
-//	@Test
-//	@Disabled("9가 고객SN 인 것이 확실하지 않으므로...")
-//	public void testDisplayCustomerAddress() {
-////		var addressList = aMan.displayCustomerAddresses(6, null);
-////		assertTrue(addressList.size() > 0);
-//	}
+	@Test
+	@Disabled("9가 고객SN 인 것이 확실하지 않으므로...")
+	public void testDisplayCustomerAddress() {
+		Logger logger = JLogger.getLogger(true);
+		var addressList = AddressMan.getCustomerAddresses(4, 1);
+		AddressMan.showCustomerAddresses(logger, addressList);
+		assertTrue(addressList.size() > 0);
+	}
 }
