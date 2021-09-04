d29eeeec0829cf696d94f8b0db7075031ad92b06
Park,JongBum <jbpark03@gmail.com>
2021-09-05 07:18:41 +0900

    변경6차,주소관리,도로명주소 검색 test case 정리
    총 4 cases, 건수: 2 cases, 목록: 2 cases

--- src/com/jbpark/dabang/module/test/Test주소관리.java
+++ src/com/jbpark/dabang/module/test/Test주소관리.java
@@ -9,21 +9,65 @@ import com.jbpark.dabang.module.AddrSearchKey;
 import com.jbpark.dabang.module.AddressMan;
 import com.jbpark.dabang.module.StopSearchingException;
 
+//@formatter:off
 class Test주소관리 {
 	
+	/** 도로명 주소 건수 검색(총 2 사례 중 1) - 검색키: 도로명/건물명
+	 * 
+	 * @throws StopSearchingException
+	 */
+	@Test
+	void testGetTotalRowsMap1() {
+		AddrSearchKey searchKey = new AddrSearchKey();
+
+		searchKey.set도로_건물("덕영대로895");
+
+		var totalRowsMap = AddressMan.getTotalRowsMap(searchKey);
+
+		System.out.println(new Gson().toJson(totalRowsMap));
+		assertEquals(totalRowsMap.get("totalRows"), 42);
+	}	
+
+	/** 도로명 주소 건수 검색(총 2 사례 중 1) - 검색키: 도로명&건물번호(=건물본번)
+	 * 
+	 * @throws StopSearchingException
+	 */
+	@Test
+	void testGetTotalRowsMap2() {
+		AddrSearchKey key건물_본번 = new AddrSearchKey();
+		
+		key건물_본번.set도로_건물("덕영대로");
+		key건물_본번.set건물본번("89");
+		
+		var totalRowsMap = AddressMan.getTotalRowsMap(key건물_본번);
+		
+		System.out.println(new Gson().toJson(totalRowsMap));
+		assertEquals(totalRowsMap.get("totalRows"), 6);
+	}
+	
+	/** 도로명 주소 목록 검색(총 2 사례 중 1) - 검색키: 도로명/건물명
+	 *  도로명 주소 건수 검색(총 2 사례 중 1) 검색 결과, pageSize 사용하여
+	 *  주소검색결과 주소 목록 크기(=2) 계산 가능	 
+	 * @throws StopSearchingException
+	 */
 	@Test
 	void testGetAddressListMap1() throws StopSearchingException {
 		AddrSearchKey searchKey = new AddrSearchKey();
 	
 		searchKey.set도로_건물("덕영대로895");
-	
+			 
 		var searchResultMap = AddressMan.getAddressListMap(
-				searchKey, 20, 3);
+				searchKey, 20, 3); // 20: pageSize
 	
 		System.out.println(new Gson().toJson(searchResultMap));
-		assertEquals(searchResultMap.get("주소검색결과").getAddresses().size(), 2);		
+		assertEquals(searchResultMap.get("주소검색결과").getAddresses().
+				size(), 2); // 2: 주소검색결과 주소 목록 크기		
 	}
-		
+	
+	/** 도로명 주소 목록 검색(총 2 사례 중 2) - 검색키: 도로명&건물번호(=건물본번)
+	 * 
+	 * @throws StopSearchingException
+	 */	
 	@Test
 	void testGetAddressListMap2() throws StopSearchingException {
 		AddrSearchKey searchKey = new AddrSearchKey();
@@ -35,43 +79,9 @@ class Test주소관리 {
 				searchKey, 20, 1);
 		
 		System.out.println(new Gson().toJson(searchResultMap));
-		assertEquals(searchResultMap.get("주소검색결과").getAddresses().size(), 20);		
+		assertEquals(searchResultMap.get("주소검색결과").getAddresses().
+				size(), 6);		
 	}
-	
-	@Test
-	void testGetTotalRowsMap1() {
-		AddrSearchKey searchKey = new AddrSearchKey();
+	//@formatter:on
 
-		searchKey.set도로_건물("덕영대로895");
-
-		var totalRowsMap = AddressMan.getTotalRowsMap(searchKey);
-
-		System.out.println(new Gson().toJson(totalRowsMap));
-		assertEquals(totalRowsMap.get("totalRows"), 42);
-	}
-
-	@Test
-	void testGetTotalRowsMap2() {
-		AddrSearchKey key건물 = new AddrSearchKey();
-		
-		key건물.set도로_건물("세진");
-		
-		var totalRowsMap = AddressMan.getTotalRowsMap(key건물);
-		
-		System.out.println(new Gson().toJson(totalRowsMap));
-		assertEquals(totalRowsMap.get("totalRows"), 2);
-	}
-
-	@Test
-	void testGetTotalRowsMap3() {
-		AddrSearchKey key건물_본번 = new AddrSearchKey();
-		
-		key건물_본번.set도로_건물("덕영대로");
-		key건물_본번.set건물본번("89");
-		
-		var totalRowsMap = AddressMan.getTotalRowsMap(key건물_본번);
-		
-		System.out.println(new Gson().toJson(totalRowsMap));
-		assertEquals(totalRowsMap.get("totalRows"), 6);
-	}
 }
