23a469257170a579c8565f74593fce281777013d
Park,JongBum <jbpark03@gmail.com>
2021-09-05 07:47:47 +0900

    JUnit related errors fixed.

--- src/com/jbpark/dabang/module/test/AddressTest.java
+++ src/com/jbpark/dabang/module/test/AddressTest.java
@@ -12,25 +12,7 @@ import com.jbpark.dabang.module.StopSearchingException;
 import com.jbpark.utility.JLogger;
 
 public class AddressTest {
-	AddressMan aMan = new AddressMan();
-
 	@Test
-	public void test팔달_주소수() {
-		var key = new AddrSearchKey();
-		
-		key.set도로_건물("팔달");
-		try {
-			int count = AddressMan.getTotalRows(key);
-			var result = AddressMan.getAddressList(key, 20, 1);
-			
-			assertTrue(count == 2045 && result.size() == 20);			
-		} catch (StopSearchingException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-//	@Disabled("4가 고객SN 인 것이 확실하지 않으므로...")
 	public void testDisplayCustomerAddress() {
 		Logger logger = JLogger.getLogger(true);
 		var addressList = AddressMan.getCustomerAddresses(4, 1);
